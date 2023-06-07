90d392eef7032e8fb87fa37c18fd4ccaabf727d8
ReaganBruce <reaganbrucebirmingham@gmail.com>
2023-06-07 16:50:07 -0500

    added moment package for date field

--- src/client/components/Card.tsx
+++ src/client/components/Card.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import Moment from "react-moment";
 
 const Card = ({ projectData }: ProjectCardProps): ReactElement => {
   return (
@@ -22,6 +23,10 @@ const Card = ({ projectData }: ProjectCardProps): ReactElement => {
                     <button className="btn btn-primary">Details</button>
                   </Link>
                 </div>
+                <span className="card-actions pr-10 mt-3">
+                  Created:
+                  <Moment format="DD/MM/YYYY">{project.createdAt}</Moment>
+                </span>
               </div>
             </section>
           </div>
@@ -32,13 +37,14 @@ const Card = ({ projectData }: ProjectCardProps): ReactElement => {
 };
 
 type ProjectCardProps = {
-  projectData?: Array<ProjectCardBody>
-}
+  projectData?: Array<ProjectCardBody>;
+};
 
 type ProjectCardBody = {
   _id: string;
+  createdAt: string;
   projectName: string;
   projectDesc: string;
   projectImg: string;
-}
+};
 export default Card;
